4d673c6979e00d92b9de38ec25024f46cc563698
Ameth Ordoñez <amethgabriel@hotmail.com>
2022-10-04 23:07:12 -0500

    finished messages sending

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,1 +1,39 @@
 const socket = io();
+
+const btnSend = document.getElementById("send-message");
+const allMessages = document.getElementById("all-messages");
+const messageText = document.getElementById("message");
+
+const username = document.cookie.replace(
+  /(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/,
+  "$1"
+);
+
+btnSend.addEventListener("click", () => {
+  socket.emit("message", {
+    user: username,
+    message: messageText.value,
+  });
+
+  messageText.value = "";
+});
+
+socket.on("message", (data) => {
+  const { user, message } = data;
+
+  const msg = document.createRange()
+    .createContextualFragment(`<div class="message">
+            <div class="image-container">
+                <img src="/images/profile.jpg" alt="">
+            </div>
+            <div class="message-body">
+                <div class="user-info">
+                    <span class="username">${user}</span>
+                    <span class="time">Hace 1 segundo</span>
+                </div>
+                <p>${message}</p>
+            </div>
+        </div>`);
+
+  allMessages.append(msg);
+});
--- src/realTimeServer.js
+++ src/realTimeServer.js
@@ -4,6 +4,17 @@ module.exports = (httpServer) => {
   const io = new Server(httpServer);
 
   io.on("connection", (socket) => {
-    console.log(socket.id);
+    // console.log(socket.id);
+
+    const cookie = socket.handshake.headers.cookie;
+    const username = cookie.split("=").pop();
+    // console.log(cookie);
+
+    socket.on("message", ({ user, message }) => {
+      io.emit("message", {
+        user: username,
+        message,
+      });
+    });
   });
 };
